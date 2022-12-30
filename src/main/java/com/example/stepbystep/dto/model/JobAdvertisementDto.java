33d74ea2aad33e8d24c8f5c7daf126d52b8cd080
Yusuf Kilic <yuzarsifkilic@gmail.com>
2022-12-30 22:51:20 +0300

    changed JobTitleDto from JobAdvertisementDto and deleted UniversityDto, MajorDto and JobTitleDto and their converter classes

--- src/main/java/com/example/stepbystep/dto/model/JobAdvertisementDto.java
+++ src/main/java/com/example/stepbystep/dto/model/JobAdvertisementDto.java
@@ -1,9 +1,11 @@
 package com.example.stepbystep.dto.model;
 
+import com.example.stepbystep.model.JobTitle;
+
 public class JobAdvertisementDto {
 
     private JobAdvertisementEmployerDto jobAdvertisementEmployerDto;
-    private JobTitleDto jobTitleDto;
+    private JobTitle jobTitle;
     private boolean isActive;
     private int minSalary;
     private int maxSalary;
@@ -11,21 +13,26 @@ public class JobAdvertisementDto {
 
     public JobAdvertisementDto() {}
 
-    public JobAdvertisementDto(JobAdvertisementEmployerDto jobAdvertisementEmployerDto, JobTitleDto jobTitleDto, boolean isActive, int minSalary, int maxSalary, int positionCount) {
+    public JobAdvertisementDto(JobAdvertisementEmployerDto jobAdvertisementEmployerDto,
+                               JobTitle jobTitle,
+                               boolean isActive,
+                               int minSalary,
+                               int maxSalary,
+                               int positionCount) {
         this.jobAdvertisementEmployerDto = jobAdvertisementEmployerDto;
-        this.jobTitleDto = jobTitleDto;
+        this.jobTitle = jobTitle;
         this.isActive = isActive;
         this.minSalary = minSalary;
         this.maxSalary = maxSalary;
         this.positionCount = positionCount;
     }
 
-    public JobTitleDto getJobTitleDto() {
-        return jobTitleDto;
+    public JobTitle getJobTitleDto() {
+        return jobTitle;
     }
 
-    public void setJobTitleDto(JobTitleDto jobTitleDto) {
-        this.jobTitleDto = jobTitleDto;
+    public void setJobTitleDto(JobTitle jobTitle) {
+        this.jobTitle = jobTitle;
     }
 
     public boolean isActive() {
