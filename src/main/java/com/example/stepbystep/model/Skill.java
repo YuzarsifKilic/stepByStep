6417431bda99d559ff2154e4f830fa5a24045088
Yusuf Kilic <yuzarsifkilic@gmail.com>
2023-01-20 15:01:33 +0300

    fixed error for skill class

--- src/main/java/com/example/stepbystep/model/Skill.java
+++ src/main/java/com/example/stepbystep/model/Skill.java
@@ -1,12 +1,18 @@
 package com.example.stepbystep.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Skill {
 
+    @Id
+    @GeneratedValue
     private int id;
     private String skillName;
     @OneToMany()
